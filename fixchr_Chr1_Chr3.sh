d9d17cb021a94952dfba7e7e0818be626eb31e83
tom-murray98 <118746667+tom-murray98@users.noreply.github.com>
2023-08-10 14:53:49 +0100

    Update and rename fixchr_Chr1.sh to fixchr_Chr1_Chr3.sh

---
+++ fixchr_Chr1_Chr3.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=fixchr_Chr1_exc_devq
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=179g
+#SBATCH --time=1:00:00
+#SBATCH --output=./%x.out
+#SBATCH --error=./%x.err
+
+source $HOME/.bash_profile
+mamba activate fixchr
+
+# In order to filter .paf alignment files so only the homologous chromosomes were represented, the grep grep -e command was used to extract both homologs twice on the same file
+# e.g 1.)  grep -e Cexcelsa_scaf_1[[:space:]]  excelsa-danica_eqx.paf > excelsa-danica_eqx_Chr1.paf
+# 2.) grep -e ptg000026[[:space:]] excelsa-danica_eqx_Chr1.paf > excelsa-danica_eqx_Chr1_ptg000026.paf
+
+
+# Fixchr for excelsa-danica alignment file between Chr1 homologs
+fixchr -c /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/excelsa-danica_eqx_Chr1_ptg000026.paf -F P -r /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/C_excelsa_V5.fa -q /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/Danica_hifi_c4_dups60.asm.bp.p_ctg.fa
+
+# Fixchr for danica-groenlandica alignment file between Chr1 homologs
+fixchr -c /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/danica-groenlandica_eqx_ptg000026l_Scaf_347.paf -F P -r /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/C_excelsa_V5.fa -q /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/Danica_hifi_c4_dups60.asm.bp.p_ctg.fa
+
+# Fixchr for excelsa-danica alignment file between Chr3 homologs
+fixchr -c /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/excelsa-danica_eqx_Chr3_ptg000017l.paf -F P -r /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/C_excelsa_V5.fa -q /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/Danica_hifi_c4_dups60.asm.bp.p_ctg.fa
+
+# Fixchr for danica-groenlandica alignment file between Chr3 homologs
+fixchr -c /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/danica-groenlandica_eqx_ptg000017l_Scaf_114.paf -F P -r /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/C_excelsa_V5.fa -q /gpfs01/home/mbxtm2/Ind_Project/ref_genomes/Danica_hifi_c4_dups60.asm.bp.p_ctg.fa
